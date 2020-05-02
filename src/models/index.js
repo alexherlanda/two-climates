43d28fa9f0d146d7a88a1aece29fb49a4167565c
Alexis Herlanda <alexisherlanda@gmail.com>
2020-05-01 20:33:42 -0500

    Updates places data model

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,47 @@
 export const alexisPlaces = [
-  { label: 'Cuautitlán Izcalli, Estado de México', id: 'a', value: 'a' },
-  { label: 'Tepotzotlán, Estado de México', id: 'b', value: 'b' },
-  { label: 'Polanco, Ciudad de México', id: 'c', value: 'c' },
+  {
+    id: 'a',
+    label: 'Cuautitlán Izcalli, Estado de México',
+    value: 'a',
+    lat: '19.42',
+    lon: '101.92',
+  },
+  {
+    id: 'b',
+    label: 'Tepotzotlán, Estado de México',
+    value: 'b',
+    lat: '19.42',
+    lon: '101.90',
+  },
+  {
+    id: 'c',
+    label: 'Polanco, Ciudad de México',
+    value: 'c',
+    lat: '19.42',
+    lon: '101.90',
+  },
 ];
 
 export const itzelPlaces = [
-  { label: 'Morelia, Michoacán', id: 'a', value: 'a' },
-  { label: 'Ziracuaretiro, Michoacán', id: 'b', value: 'b' },
-  { label: 'Uruapan, Michoacán', id: 'c', value: 'c' },
+  {
+    id: 'a',
+    label: 'Morelia, Michoacán',
+    value: 'a',
+    lat: '19.42',
+    lon: '101.90',
+  },
+  {
+    id: 'b',
+    label: 'Ziracuaretiro, Michoacán',
+    value: 'b',
+    lat: '19.42',
+    lon: '101.91',
+  },
+  {
+    id: 'c',
+    label: 'Uruapan, Michoacán',
+    value: 'c',
+    lat: '19.42',
+    lon: '101.90',
+  },
 ];
