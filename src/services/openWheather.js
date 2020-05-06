2982dad75b118a093e5adecb42ceb6bf659ac494
Alexis Herlanda <alexisherlanda@gmail.com>
2020-05-05 20:30:27 -0500

    better usable services

--- src/services/openWheather.js
+++ src/services/openWheather.js
@@ -1,8 +1,12 @@
-import { getRequest } from './requests';
+import request from './requests';
 
-export function getPlaceClimate(params) {
-  getRequest({
-    url: 'https://jsonplaceholder.typicode.com/todoss',
-    params: params,
+export async function getClimateByCityId(params) {
+  return request('', {
+    method: 'GET',
+    params: {
+      ...params,
+      //! App id is not public. Use your own appId for OpenWhetner
+      appid: '418eb025a5987a2c2125c3d46b58ba9b',
+    },
   });
 }
