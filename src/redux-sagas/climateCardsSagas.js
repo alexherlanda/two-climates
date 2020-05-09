e1b01dac8b7348b20f72220a0448ca3798dfa129
Alexis Herlanda <alexisherlanda@gmail.com>
2020-05-09 16:22:44 -0500

    Enhances saga to take all actions and format of this response

--- src/redux-sagas/climateCardsSagas.js
+++ src/redux-sagas/climateCardsSagas.js
@@ -1,29 +1,45 @@
 import { getClimateByCityId } from '../services/openWheather';
-import { takeLatest, put, call } from 'redux-saga/effects';
+import { takeEvery, put, call } from 'redux-saga/effects';
 import {
   ACTIVE_PLACE_REQUEST,
   activePlaceSucess,
   activePlaceFail,
 } from '../redux/actions/cardClimateActions';
 
+function validateData(value) {
+  let valueToAssing;
+  if (typeof value === 'undefined') {
+    valueToAssing = false;
+  } else {
+    valueToAssing = value;
+  }
+  return valueToAssing;
+}
+
+function removeDecimals(value) {
+  let formatedNumber = Number(value);
+  return formatedNumber.toFixed(0);
+}
+
 function* requestActivePlaceAsync(action) {
   let dataToSave;
   try {
-    const response = yield call(getClimateByCityId, { id: 3979654 });
+    const response = yield call(getClimateByCityId, { id: action.payload.cityId });
     if (typeof response !== 'undefined' && response.status === 200) {
       dataToSave = {
         mainCard: {
           ...response.data.weather[0],
+          temp: validateData(removeDecimals(response.data.main.temp)),
         },
         dataGrid: {
-          humidity: response.data.main.humidity,
-          sunrise: response.data.sys.sunrise,
-          sunset: response.data.sys.sunrise,
-          temp_min: response.data.main.temp_min,
-          tempMax: response.data.main.temp_max,
+          rain: validateData(response.data.rain),
+          humidity: validateData(removeDecimals(response.data.main.humidity)),
+          sunrise: validateData(response.data.sys.sunrise),
+          sunset: validateData(response.data.sys.sunset),
+          tempMin: validateData(removeDecimals(response.data.main.temp_min)),
+          tempMax: validateData(removeDecimals(response.data.main.temp_max)),
         },
       };
-      console.log('dataToSave :>> ', dataToSave);
       yield put(
         activePlaceSucess({ cardId: action.payload.cardId, data: dataToSave })
       );
@@ -38,5 +54,5 @@ function* requestActivePlaceAsync(action) {
 
 export function* watchRequestMarkets() {
   console.log('ACTIVE_PLACE_REQUEST :>> ', ACTIVE_PLACE_REQUEST);
-  yield takeLatest(ACTIVE_PLACE_REQUEST, requestActivePlaceAsync);
+  yield takeEvery(ACTIVE_PLACE_REQUEST, requestActivePlaceAsync);
 }
