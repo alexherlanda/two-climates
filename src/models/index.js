29d54bb3adcd1ecb2fb52d5966c177b193741138
Alexis Herlanda <alexisherlanda@gmail.com>
2020-05-09 17:50:21 -0500

    updates Model

--- src/models/index.js
+++ src/models/index.js
@@ -2,46 +2,28 @@ export const alexisPlaces = [
   {
     id: '3515807',
     label: 'Cuautitlán Izcalli, Estado de México',
-    value: 'a',
-    lat: '19.42',
-    lon: '101.92',
   },
   {
-    id: 'p02',
+    id: '3515827',
     label: 'Tepotzotlán, Estado de México',
-    value: 'b',
-    lat: '19.42',
-    lon: '101.90',
   },
   {
-    id: 'p03',
+    id: '3521305',
     label: 'Polanco, Ciudad de México',
-    value: 'c',
-    lat: '19.42',
-    lon: '101.90',
   },
 ];
 
 export const itzelPlaces = [
   {
-    id: 'p04',
+    id: '3995402',
     label: 'Morelia, Michoacán',
-    value: 'a',
-    lat: '19.42',
-    lon: '101.90',
   },
   {
     id: '3979654',
     label: 'Ziracuaretiro, Michoacán',
-    value: 'b',
-    lat: '19.42',
-    lon: '101.91',
   },
   {
-    id: 'p05',
+    id: '3980760',
     label: 'Uruapan, Michoacán',
-    value: 'c',
-    lat: '19.42',
-    lon: '101.90',
   },
 ];
